c1922033009aac7e8a6633c648bf2bfe6ac4a6e8
VW-Media <72121063+VW-Media@users.noreply.github.com>
2020-09-30 00:48:54 -0500

    Add files via upload

---
+++ build/precache-manifest.dbce19ddb910820fae96c4b8a09988bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77fafb7408ba5aa4fc15ea35b8a686c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae552c90c4d0bdae81d8",
+    "url": "/static/css/main.30b6cfdb.chunk.css"
+  },
+  {
+    "revision": "7114a4ef1c635bf29e71",
+    "url": "/static/js/2.082288d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.082288d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae552c90c4d0bdae81d8",
+    "url": "/static/js/main.1649f22c.chunk.js"
+  },
+  {
+    "revision": "9b7a4aff4053a7ba4646",
+    "url": "/static/js/runtime-main.7ab2c3eb.js"
+  },
+  {
+    "revision": "9f2a1534453c7532426bac0e3525d7dc",
+    "url": "/static/media/cover.9f2a1534.jpg"
+  }
+]);
